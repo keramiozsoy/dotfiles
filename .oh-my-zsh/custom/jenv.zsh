6d1a27f22dd8355ee3f3e03841d6c468c7a33e6f
kerami <keramidev@gmail.com>
2024-10-08 21:52:48 +0300

    updates jenv'

--- .oh-my-zsh/custom/jenv.zsh
+++ .oh-my-zsh/custom/jenv.zsh
@@ -1,2 +1,12 @@
 export PATH="$HOME/.jenv/bin:$PATH"
 eval "$(jenv init -)"
+
+
+# import all installed java versions
+for dir in /Library/Java/JavaVirtualMachines/*; do eval "jenv add $dir/Contents/Home/"; done
+
+
+# Select minimum version of java as global version of PC.
+jenv versions --bare | head -n 1 | xargs jenv global
+
+jenv doctor
